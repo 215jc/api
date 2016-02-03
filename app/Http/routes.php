d33ab862068b9b50f8737e06eea200fe34758804
Jason Curran <jc@jasoncurran.com>
2016-02-03 02:42:20 +0000

    add Api routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,3 +29,47 @@ Route::get('/', function () {
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Api Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+
+$api = app('Dingo\Api\Routing\Router');
+
+// Version 1 of our API
+$api->version('v1', function ($api) {
+
+	// Set our namespace for the underlying routes
+	$api->group(['namespace' => 'Api\Controllers', 'middleware' => 'cors'], function ($api) {
+
+		// Login route
+		$api->post('login', 'AuthController@authenticate');
+		$api->post('register', 'AuthController@register');
+
+		// Dogs! All routes in here are protected and thus need a valid token
+		//$api->group( [ 'protected' => true, 'middleware' => 'jwt.refresh' ], function ($api) {
+		$api->group( [ 'middleware' => 'jwt.refresh' ], function ($api) {
+
+			$api->get('users/me', 'AuthController@me');
+			$api->get('validate_token', 'AuthController@validateToken');
+			
+			$api->get('dogs', 'DogsController@index');
+			$api->post('dogs', 'DogsController@store');
+			$api->get('dogs/{id}', 'DogsController@show');
+			$api->delete('dogs/{id}', 'DogsController@destroy');
+			$api->put('dogs/{id}', 'DogsController@update');
+
+		});
+
+	});
+
+});
+
+
+
+
